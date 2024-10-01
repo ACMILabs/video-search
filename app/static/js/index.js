3e7f253f152421828763c7fb563f3f060652f043
Simon Loffler <simon.loffler@acmi.net.au>
2024-10-01 12:11:25 +0930

    #3 Add information text to the homepage and fix the dropdown alignment bug on Safari/Chrome/Firefox

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -11,3 +11,14 @@ function setPlaybackTime(videoId, seconds) {
   video.currentTime = seconds - 0.3;
   video.play();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var isSafariMac = /Safari/.test(navigator.userAgent) &&
+                  !/Mobile/.test(navigator.userAgent) &&
+                  /Apple Computer/.test(navigator.vendor);
+
+  const select = document.getElementsByTagName('select')[0];
+  if (isSafariMac && select) {
+    select.classList.add('macos-safari');
+  }
+}, false);
